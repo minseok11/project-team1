b7d7de2967acb106280bab66b96ad6e11796bcbe
minseok11 <sw50606@hanmir.com>
2017-01-10 17:56:33 +0900

    content.jsp수정

--- semi_project/src/semi_project/Starter.java
+++ semi_project/src/semi_project/Starter.java
@@ -1,18 +1,25 @@
 package semi_project;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import shopping.dao.CategoryDao;
+import shopping.dto.CategoryDTO;
 @WebServlet("/starter.do")
 public class Starter extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		req.setAttribute("head", "/defaultHeader.jsp");
 		req.setAttribute("content", "/defaultContent.jsp");
+		CategoryDao dao=new CategoryDao();
+		ArrayList<CategoryDTO> list=dao.list();
+		req.setAttribute("list",list);
 		req.getRequestDispatcher("/index.jsp").forward(req, res);
 		
 	}
